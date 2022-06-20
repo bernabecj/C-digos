0e3eedcbd5f914513380c892c11850ff6a2640f1
bernabecj <62186957+bernabecj@users.noreply.github.com>
2022-06-20 15:48:37 -0500

    TwoSum

---
+++ LeetCode/TwoSum/main.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if(nums[i] + nums[j] == target):
+                print(i, j)
+
+nums = [3,2,4]
+target = 6
+
+twoSum(nums, target)
